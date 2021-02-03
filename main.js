9fb666a0024acff01512bdfd0a8356b8e311c9e8
Juyeon Kim <jay.jykim91@gmail.com>
2021-02-03 20:06:09 +0900

    1차 정리

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-// $( '.colored-text' ).css( 'color', 'red' );
-jQuery(document).ready(function ($) {
-  $('.number').counterUp({
-    delay: 10,
-    time: 1300
-  });
+// coun-up Start
+$('.number').counterUp({
+  delay: 10,
+  time: 1300
 });
+// coun-up End
 
+
+
+// Progressive loading End
 const observer = lozad();
 observer.observe();
+// Progressive loading Start
 
-const header = document.querySelector('.number');
 
-header.addEventListener('click', (event) => {
-  console.log('in!');
-})
+
+// Image appearance upon scrolling Start
+const featureTextUp = {
+  distance: '30px',
+  origin: 'bottom',
+  duration: 1500,
+};
+const featureImageUp = {
+  distance: '30px',
+  origin: 'bottom',
+  duration: 2500,
+};
+const reviewUp = {
+  delay: 0,
+  interval: 100,
+  distance: '30px',
+  origin: 'bottom',
+  duration: 1500,
+};
+ScrollReveal().reveal('.feature-text', featureTextUp);
+ScrollReveal().reveal('.feature-image', featureImageUp);
+ScrollReveal().reveal('.reviews .review', reviewUp);
+// Image appearance upon scrolling End
